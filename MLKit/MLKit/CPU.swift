0e728a98de80f7c45ba711794d176e0c2c909832
kesavm <kesav.mulakaluri@gmail.com>
2015-12-22 14:40:36 -0800

    Added CPU class

---
+++ MLKit/MLKit/CPU.swift
@@ -0,0 +1,28 @@
+//
+//  CPU.swift
+//  MLKit
+//
+//  Created by Kesav Mulakaluri on 12/22/15.
+//  Copyright © 2015 Kesav Mulakaluri. All rights reserved.
+//
+
+import Foundation
+
+class CPU: MLComputeDevice {
+    
+    func addMatrices(a a: Matrix, b: Matrix) -> Matrix {
+        return a
+    }
+    
+    func subtractMatrices(a a: Matrix, b: Matrix) -> Matrix {
+        return a
+    }
+    
+    func multiplyMatrices(a a: Matrix, b: Matrix) -> Matrix {
+        return a
+    }
+    
+    func scaleMatrix(a: Matrix, by c: Float) -> Matrix {
+        return a
+    }
+}
