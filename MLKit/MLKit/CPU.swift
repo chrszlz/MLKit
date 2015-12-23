8dcf2cdf4793575009e7c22d62e9c686ac90a975
kesavm <kesav.mulakaluri@gmail.com>
2015-12-22 16:53:34 -0800

    Implemented Matrix operations in CPU

--- MLKit/MLKit/CPU.swift
+++ MLKit/MLKit/CPU.swift
@@ -7,22 +7,42 @@
 //
 
 import Foundation
+import Accelerate
 
+/// CPU is a light weight wrapper around the Accelerate framework to perform
+/// fast matrix computations.
 class CPU: MLComputeDevice {
     
+    /// Returns `a` + `b`.
     func addMatrices(a a: Matrix, b: Matrix) -> Matrix {
-        return a
+        var res = b
+        cblas_saxpy(Int32(res.elements.count), 1.0, a.elements, 1, &res.elements, 1)
+ 
+        return res
     }
     
+    /// Returns `a` - `b`.
     func subtractMatrices(a a: Matrix, b: Matrix) -> Matrix {
-        return a
+        let negative_b = scaleMatrix(b, by: -1.0)
+        let res = addMatrices(a: a, b: negative_b)
+
+        return res
     }
     
+    /// Returns `a` * `b`.
     func multiplyMatrices(a a: Matrix, b: Matrix) -> Matrix {
-        return a
+        var res = Matrix.zeros(a.rows, columns: b.columns)
+        cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, Int32(a.rows), Int32(b.columns), Int32(a.columns), 1.0, a.elements, Int32(a.columns), b.elements, Int32(b.columns), 0.0, &res.elements, Int32(res.columns))
+
+        return res
     }
     
+    /// Multiplies each element in `a` by `c`.
     func scaleMatrix(a: Matrix, by c: Float) -> Matrix {
-        return a
+        var res = a
+        var alpha = c
+        cblas_cscal(Int32(a.elements.count), &alpha, &res, 1)
+        
+        return res
     }
 }
