c5d8d9e21da5f89d913773946e10a51fa7582b6a
kesavm <kesav.mulakaluri@gmail.com>
2015-12-27 14:56:55 -0800

    Added Layer protocol.

---
+++ MLKit/MLKit/Layer.swift
@@ -0,0 +1,25 @@
+//
+//  Layer.swift
+//  MLKit
+//
+//  Created by Kesav Mulakaluri on 12/27/15.
+//  Copyright © 2015 Kesav Mulakaluri. All rights reserved.
+//
+
+import Foundation
+
+/// Represents a layer of a Neural Network.
+public protocol Layer: MLBlock {
+    
+    /// The number of incoming connections for the layer.
+    var numInputs: Int { get set }
+    
+    /// The number of outgoing connections for the layer.
+    var numOutputs: Int { get set }
+    
+    /// The weights for this layer.
+    var weights: Matrix { get set }
+    
+    /// The bias for this layer.
+    var bias: Matrix { get set }
+}
