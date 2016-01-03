5eed54ac6deb3dfcca5ec2cde2820c7c4d24eb4d
Chris Zelazo <chris@zelazo.net>
2016-01-02 22:47:37 -0600

    More printing

--- MLKit.playground/Pages/CPU GPU Validation.xcplaygroundpage/Contents.swift
+++ MLKit.playground/Pages/CPU GPU Validation.xcplaygroundpage/Contents.swift
@@ -10,15 +10,24 @@ let m: Matrix = [[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]]
 MLSetComputeMode(.CPU)
 
 let addCPU  = m + m
+print(addCPU)
 let subCPU  = m - m
+print(subCPU)
 let multCPU = m * 3
+print(multCPU)
+print(m)
+let vecMultCPU = m * m
+print(vecMultCPU)
 
 // GPU Mode
 MLSetComputeMode(.GPU)
 
 let addGPU  = m + m
+print(addGPU)
 let subGPU  = m - m
+print(subGPU)
 let multGPU = 3 * m
+print(multGPU)
 
 // Validate
 // If all evaluate to true, CPU & GPU calculations are consistent
